d0217ba3848e3e879f9b3d746e93c93dfb2bb119
Lycaon <lycaonj@gmail.com>
2014-02-17 07:51:19 -0600

    Hid item description unless holding shift.

--- main/java/net/ixios/advancedthaumaturgy/items/ItemEngine.java
+++ main/java/net/ixios/advancedthaumaturgy/items/ItemEngine.java
@@ -2,6 +2,8 @@ package net.ixios.advancedthaumaturgy.items;
 
 import java.util.List;
 
+import org.lwjgl.input.Keyboard;
+
 import net.ixios.advancedthaumaturgy.AdvThaum;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemBlock;
@@ -33,6 +35,10 @@ public class ItemEngine extends ItemBlock
 	@Override
 	public void addInformation(ItemStack stack,	EntityPlayer player, List list, boolean par4)
 	{
+    	boolean shiftdown = Keyboard.isKeyDown(Keyboard.KEY_LSHIFT) || Keyboard.isKeyDown(Keyboard.KEY_RSHIFT);
+    	if (!shiftdown)
+    		return;
+    		
 		String desc = StatCollector.translateToLocal("tile.at.essentiaengine.desc");
 		String[] lines = desc.split("\\|");
 		for (String s : lines)
