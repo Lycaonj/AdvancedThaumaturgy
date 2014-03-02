dcf8569d9f83dba405fb062b38042e3f73b556be
Lycaon <lycaonj@gmail.com>
2014-03-02 01:10:40 -0600

    Began work on a 'quarry'

--- main/java/net/ixios/advancedthaumaturgy/tileentities/TileExcavator.java
+++ main/java/net/ixios/advancedthaumaturgy/tileentities/TileExcavator.java
@@ -1,6 +1,35 @@
 package net.ixios.advancedthaumaturgy.tileentities;
 
-public class TileExcavator
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.world.World;
+
+import org.lwjgl.util.Color;
+
+import thaumcraft.common.items.wands.ItemWandCasting;
+
+public class TileExcavator extends TileMicrolithBase
 {
+
+	public TileExcavator(Color color)
+    {
+	    super(color);
+	    // TODO Auto-generated constructor stub
+    }
+
+	@Override
+    public boolean onBlockActivated(World world, int x, int y, int z,
+            EntityPlayer player, int side, float hitX, float hitY, float hitZ)
+    {
+		if ((player.getHeldItem() != null) && (player.getHeldItem().getItem() instanceof ItemWandCasting))
+		{
+			// do a search for the 4 corners and activate if a square is formed, do maybe 64
+		}
+		else // toggle the finder beams ala landmarks
+		{
+			
+		}
+		
+	    return false;
+    }
 
 }
